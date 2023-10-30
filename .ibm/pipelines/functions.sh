7a19b533a06320d7009a72170a3e8dd30c64e33f
Joseph Kim <joskim@redhat.com>
2023-10-30 17:17:54 -0400

    set content type with utf-8 charset
    Signed-off-by: Joseph Kim <joskim@redhat.com>

--- .ibm/pipelines/functions.sh
+++ .ibm/pipelines/functions.sh
@@ -25,10 +25,7 @@ save_logs() {
         STATUS="with errors"
     fi
 
-    GITHUB_REPOSITORY_NAME="${GITHUB_REPOSITORY#*/}"
-    echo "Repository Name: $GITHUB_REPOSITORY_NAME"
-
-    cat <<EOF | pr-commenter -key-from-env-var ROBOT_KEY -application-id=${GITHUB_APP_PR_COMMENTER_ID} -pr-comment=${GIT_PR_NUMBER} -repository=${GITHUB_REPOSITORY_NAME} -org=${GITHUB_REPOSITORY_OWNER}
+    cat <<EOF | pr-commenter -key-from-env-var ROBOT_KEY -application-id=${GITHUB_APP_PR_COMMENTER_ID} -pr-comment=${GIT_PR_NUMBER} -repository=${GITHUB_REPOSITORY_NAME} -org=${GITHUB_ORG_NAME}
 ${NAME} on commit ${GIT_COMMIT} finished ${STATUS}.
 View [test log](${BASE_URL}/${LOGFILE}.html)
 EOF
